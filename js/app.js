91bde587e5069d738b92fe4f60f5588ebee0963f
Troy Barnard <troy.w.barnard@gmail.com>
2015-11-20 01:59:54 -0800

    all good

---
+++ js/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('TroyApp' , ['ngSanitize', 'ui.router', 'ui.bootstrap'])
+    .config(function($stateProvider) {
+        $stateProvider.
+            state('home', {
+                url: '/',
+                templateUrl: 'partials/home.html',
+                controller: 'TroyCtrl'
+            })
+            .state('resume', {
+                url: '/resume',
+                templateUrl: 'partials/resume.html',
+                controller: 'TroyCtrl'
+
+            })
+            .state('blog', {
+                url: '/blog',
+                templateUrl: 'partials/blog.html',
+                controller: 'TroyCtrl'
+            })
+            .state('videos', {
+                url: '/videos',
+                templateUrl: 'partials/videos.html',
+                controller: 'TroyCtrl'
+            })
+    })
+    .config(function($urlRouterProvider){
+        // if the path doesn't match any of the urls you configured
+        // otherwise will take care of routing the user to the specified url
+        $urlRouterProvider.otherwise('/');
+    })
+    .controller('TroyCtrl', ['$scope', '$http', function($scope, $http) {
+
+}]);
